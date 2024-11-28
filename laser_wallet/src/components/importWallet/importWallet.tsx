83cd323de6cc5da438e071009536c347584d9413
Toastx <toastx.sol@gmail.com>
2024-11-29 04:04:44 +0530

    added import key using seed phrase, todo privatekey

--- laser_wallet/src/components/importWallet/importWallet.tsx
+++ laser_wallet/src/components/importWallet/importWallet.tsx
@@ -1,5 +1,50 @@
-function importWallet() {
-  return <div>ImportWallet</div>;
-}
+import { createSignal } from "solid-js";
+import { Keypair } from '@solana/web3.js';
+import {mnemonicToSeed} from '@scure/bip39';
 
-export default importWallet;
+
+import bs58 from 'bs58';
+
+
+const importWallet = () => {
+  const [seedPhrase, setSeedPhrase] = createSignal<string>("");
+  const [keypair, setKeypair] = createSignal<Keypair | null>(null);
+  const generateWallet = async () => {
+    
+    console.log('seedPhrase:', seedPhrase());
+    const seed =  mnemonicToSeed(seedPhrase());
+    console.log('seed:', seed);
+
+    const keypair = Keypair.fromSeed((await seed).slice(0, 32))
+    console.log(bs58.encode(keypair.secretKey))
+    console.log('keypair:', keypair);
+    setKeypair(keypair);
+  };
+  return (
+    <div>
+      <h2>Import Solana Wallet</h2>
+      <form
+        class="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          generateWallet();
+        }}
+      >
+        <input
+          id="greet-input"
+          placeholder="Enter the seed phrase"
+          onChange={(e) => setSeedPhrase(e.currentTarget.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+    {seedPhrase() && (
+        <div >
+          <h3>Wallet Details</h3>
+          <p><strong>Public Key:</strong> {keypair()?.publicKey.toBase58()}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default importWallet
